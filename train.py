16c1637065fe2b4928ab3664039db334e91bdc8d
Ubuntu <ubuntu@ip-172-31-70-236.ec2.internal>
2024-06-20 04:19:34 +0000

    code update, model update(amp,tensorboard)

--- train.py
+++ train.py
@@ -20,8 +20,21 @@ def train(opt):
     num_workers = min([os.cpu_count(), batch_size])
     train_dataloader = torch.utils.data.DataLoader(train_dataset,batch_size=batch_size, shuffle = True, 
                                                    num_workers=num_workers,drop_last = False)
+    
+    val_dataset = torchvision.datasets.CIFAR10('./data',train=False, download=False,transform=transforms)
+    val_dataloader = torch.utils.data.DataLoader(val_dataset,batch_size=batch_size, shuffle = True, 
+                                                   num_workers=num_workers,drop_last = True)
+
+                                            
     #Network Model
     model = vgg11_bn()
+
+    #GPU-support
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.device_count() > 1: #multi-GPU
+        model = torch.nn.DataParallel(model)
+    model.to(device)
+    print(torch.cuda.device_count())
 
     #Loss function ans optimizer
     loss_fn = nn.CrossEntropyLoss()
@@ -33,7 +46,7 @@ def train(opt):
     for epoch in range(start_epoch, end_epoch):
         print('batch idx=%d/%d' % (epoch, end_epoch-1))
         t0 = time.time()
-        epoch_loss = train_one_epoch(train_dataloader,model,loss_fn,optimizer)
+        epoch_loss = train_one_epoch(train_dataloader,model,loss_fn,optimizer,device)
         t1 = time.time()
         print("loss=%.4f (took %.2f sec)\n" %(epoch_loss,t1-t0))
 
@@ -45,10 +58,11 @@ def train(opt):
 
     pass #debug breakpoint
 
-def train_one_epoch(train_dataloader, model, loss_fn, optimizer):
+def train_one_epoch(train_dataloader, model, loss_fn, optimizer,device):
     model.train()
     losses=[]
     for i, (imgs, targets) in enumerate(train_dataloader):
+        imgs,targets = imgs.to(device), targets.to(device)
         preds = model(imgs)
         loss = loss_fn(preds, targets)
         optimizer.zero_grad()
@@ -63,7 +77,7 @@ def train_one_epoch(train_dataloader, model, loss_fn, optimizer):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', type=int, default=200, help='target epochs')
-    parser.add_argument('--batch-size', type=int, default=4, help='batch size')
+    parser.add_argument('--batch-size', type=int, default=128, help='batch size')
     parser.add_argument('--name', default='ohhan', help='name for the run')
 
     opt = parser.parse_args()
